f138c275abac036f9f1e1736a71458aa1ff5ca33
Dave Thomas <opensource@peoplemerge.com>
2011-02-25 07:44:48 -0800

    Bali changes

--- src/test/java/LangTest.java
+++ src/test/java/LangTest.java
@@ -1,9 +1,34 @@
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.TokenStream;
 import org.junit.Test;
+
+import com.peoplemerge.ngds.NgdsLexer;
+import com.peoplemerge.ngds.NgdsParser;
+import com.peoplemerge.ngds.Result;
 
 public class LangTest {
 	@Test
-	public void sentence() {
-		String sentence = "Create a new environment called development using 1 nodes from dom0.";
-		
+	public void createSentence() throws RecognitionException {
+		String sentence = "Create a new environment called development using 1 small nodes from dom0.";
+		Result result = parse(sentence);
+	}
+
+	@Test
+	public void deploySentence() throws RecognitionException {
+		String sentence = "Deploy latest infrastructure code from version control to the testing environment.";
+		Result result = parse(sentence);
+		result.getCommand();
+	}
+
+	private Result parse(String sentence) throws RecognitionException {
+		CharStream stringStream = new ANTLRStringStream(sentence);
+		NgdsLexer lexer = new NgdsLexer(stringStream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		NgdsParser parser = new NgdsParser(tokenStream);
+		Result result = parser.program().result;
+		return result;
 	}
 }
