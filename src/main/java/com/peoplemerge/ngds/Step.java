72683af8c7008804d21cbb8aa8a04555394f7f30
Dave Thomas <opensource@peoplemerge.com>
2012-10-04 23:17:39 -0700

    implemented toString

--- src/main/java/com/peoplemerge/ngds/Step.java
+++ src/main/java/com/peoplemerge/ngds/Step.java
@@ -1,38 +1,38 @@
 /************************************************************************
-** 
-** Copyright (C) 2011 Dave Thomas, PeopleMerge.
-** All rights reserved.
-** Contact: opensource@peoplemerge.com.
-**
-** This file is part of the NGDS language.
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**    http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-**  
-** Other Uses
-** Alternatively, this file may be used in accordance with the terms and
-** conditions contained in a signed written agreement between you and the 
-** copyright owner.
-************************************************************************/
+ ** 
+ ** Copyright (C) 2011 Dave Thomas, PeopleMerge.
+ ** All rights reserved.
+ ** Contact: opensource@peoplemerge.com.
+ **
+ ** This file is part of the NGDS language.
+ **
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **    http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ **  
+ ** Other Uses
+ ** Alternatively, this file may be used in accordance with the terms and
+ ** conditions contained in a signed written agreement between you and the 
+ ** copyright owner.
+ ************************************************************************/
 package com.peoplemerge.ngds;
 
 import java.util.List;
 
-public class Step implements Executable{
+public class Step implements Executable {
 
 	private Executable command;
 	private AcceptsCommands node;
 	private String output;
-	
+
 	public String getOutput() {
 		return output;
 	}
@@ -59,6 +59,12 @@ public class Step implements Executable{
 		return null;
 	}
 
-	
-	
+	public String toString() {
+		if (output == null) {
+			return "Running: " + command + " on " + node;
+		} else {
+			return "Ran: " + command + " on " + node + " result: " + output;
+		}
+	}
+
 }
